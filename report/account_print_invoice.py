ada6b2f4b67181ae38f86983058593e51beefc83
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-10-10 14:05:37 +0300

    adding bid to invoice

--- report/account_print_invoice.py
+++ report/account_print_invoice.py
@@ -22,7 +22,7 @@
 import time
 from openerp.report import report_sxw
 
-class account_invoice(report_sxw.rml_parse):
+class account_invoice_finland(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(account_invoice, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
@@ -31,7 +31,7 @@ class account_invoice(report_sxw.rml_parse):
 report_sxw.report_sxw(
     'report.account.invoice',
     'account.invoice',
-    'addons/account/report/account_print_invoice.rml',
-    parser=account_invoice
+    'addons/business_id_finland/report/account_print_invoice.rml',
+    parser=account_invoice_finland
 )
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
