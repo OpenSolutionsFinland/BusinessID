ed7bd541d640415b1149094e42a48d5fc9109975
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-10-10 14:47:33 +0300

    adding bid to invoice

--- business_id.py
+++ business_id.py
@@ -1,4 +1,5 @@
 from osv import fields, osv
+from openerp.tools.translate import _
 
 class business_id(osv.osv):
 	_name='res.partner'
@@ -11,13 +12,13 @@ class business_id(osv.osv):
 
 business_id()
 
-class account_invoice(osv.osv):
-    _inherit='account.invoice'
+class account_invoice_custom(osv.osv):
     _name='account.invoice'
-
+    _inherit='account.invoice'
+    
     def invoice_print(self, cr, uid, ids, context=None):
         res = super(account_invoice, self).invoice_print( cr, uid, ids,context) #self, cr, uid, ids, context)
         res["report_name"] = "account.invoice.custom"
         return res
 
-account_invoice()
+account_invoice_custom()
