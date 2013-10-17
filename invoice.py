9dd518582dc2316bacb3fa21a4ead33f3662d043
Jesse Vartiainen <jezco@host-001.elisa>
2013-10-17 09:20:21 +0300

    overriding account.invoice print button

--- invoice.py
+++ invoice.py
@@ -1,9 +1,10 @@
-
-from osv import fields, osv
+from osv import osv, fields
 
 class account_invoice_custom(osv.osv):
     _name='account.invoice'
     _inherit='account.invoice'
+    
+    _columns = {}
     
     def invoice_print(self, cr, uid, ids, context=None):
         res = super(account_invoice_custom, self).invoice_print( cr, uid, ids,context)
